8ef6a4745edf03e7a0e46585bc595a68ed014e04
«AndriiPoltava» <Omelianenko993@gmail.com>
2022-11-21 11:38:15 +0200

    Fi

---
+++ wha-optimize.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Plugin Name: git
+ * Description: Описание плагина желательно не очень длинное (140 символов)
+ * Plugin URI:  Ссылка на страницу плагина
+ * Author URI:  Ссылка на автора
+ * Author:      Имя автора
+ * Version:     Версия плагина, например 1.0
+ *
+ * Text Domain: ID перевода, указывается в load_plugin_textdomain()
+ * Domain Path: Путь до файла перевода.
+ * Requires at least: 2.5
+ * Requires PHP: 5.4
+ *
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Network:     Укажите "true" для возможности активировать плагин для сети Multisite.
+ * Update URI: https://example.com/link_to_update
+  */
